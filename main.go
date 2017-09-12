6bda28a539aecb4cbe23b4c9972173392dac3f87
mxi4oyu <798033502@qq.com>
2017-09-12 18:33:56 +0800

    add gbk encoder and decoder

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"context"
+	"time"
+	"github.com/MXi4oyu/Utils/subprocess"
+	"github.com/MXi4oyu/Utils/gbk"
+)
+
+func main()  {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Duration(60)*time.Second)
+	defer cancel()
+
+	str,err:=subprocess.RunCommand(ctx,"sh","-c","net user")
+	if err!=nil{
+		fmt.Println(err.Error())
+	}
+	fmt.Println(gbk.Decode(str))
+}
---
+++ subprocess/subprocess.go
@@ -0,0 +1,34 @@
+package subprocess
+
+
+import (
+	"fmt"
+	"context"
+	"os/exec"
+)
+
+func RunCommand(ctx context.Context, cmd string, args ...string) (string, error) {
+
+	var c *exec.Cmd
+
+	if ctx != nil {
+		c = exec.CommandContext(ctx, cmd, args...)
+	} else {
+		c = exec.Command(cmd, args...)
+	}
+
+
+	output, err := c.Output()
+	if err != nil {
+		return string(output), err
+	}
+
+	// check for exec context timeout
+	if ctx != nil {
+		if ctx.Err() == context.DeadlineExceeded {
+			return "", fmt.Errorf("command %s timed out", cmd)
+		}
+	}
+
+	return string(output), nil
+}
