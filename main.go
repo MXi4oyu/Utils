f9d366bd87fae0ea724653ab9724addea11291cd
MXi4oyu <798033502@qq.com>
2017-10-09 15:33:49 +0800

    add walkpath

--- main.go
+++ main.go
@@ -6,17 +6,26 @@ import (
 	"time"
 	"github.com/MXi4oyu/Utils/subprocess"
 	"github.com/MXi4oyu/Utils/cnencoder/gbk"
+	"github.com/MXi4oyu/Utils/walkpath"
 )
 
 func main()  {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(60)*time.Second)
 	defer cancel()
 
-	str,err:=subprocess.RunCommand(ctx,"sh","-c","ipconfig")
+	str,err:=subprocess.RunCommand(ctx,"sh","-c","ls")
 	if err!=nil{
 		fmt.Println(err.Error())
 	}
 
 	fmt.Println(gbk.Decode(str))
+
+	//获取指定目录下的所有文件，不进入子目录，可通过后缀名过滤
+	files,err :=walkpath.ListDir("/var/www/",".php")
+	fmt.Println(files)
+
+	//获取指定目录及其子目录下的所有文件，可通过后缀名进行过滤
+	filesall,err:=walkpath.WalkDir("/var/www",".php")
+	fmt.Println(filesall)
 
 }
